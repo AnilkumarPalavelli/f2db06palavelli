14690f29b9c9de869c0cb183411fc533dd7bfbbd
AnilkumarPalavelli <111616559+AnilkumarPalavelli@users.noreply.github.com>
2022-11-09 01:21:49 -0600

    modified fish.js in controller

--- controllers/fish.js
+++ controllers/fish.js
@@ -43,3 +43,23 @@ exports.fish_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+    // Handle Costume create on POST.
+exports.fish_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new fish();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    
+    document.fish_type = req.body.fish_type;
+    document.fish_weight = req.body.fish_weight;
+    document.fish_cost = req.body.fish_cost;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
