0a99c584b6ab081017e95fa8d265f307689488eb
AnilkumarPalavelli <111616559+AnilkumarPalavelli@users.noreply.github.com>
2022-11-11 00:13:55 -0600

    modified fish.js in controllers

--- app.js
+++ app.js
@@ -13,44 +13,47 @@ var fish = require("./models/fish");
 var resourceRouter = require('./routes/resource');
 
 // We can seed the collection if needed on server start
-async function recreateDB(){
+async function recreateDB() {
   // Delete everything
   await fish.deleteMany();
-  let instance1 = new fish({fish_type:"Salman",fish_weight:"2lbs",fish_cost:34});
-  let instance2 = new fish({fish_type:"Sardine",fish_weight:"6lbs",fish_cost:23})
-  let instance3 = new fish({fish_type:"Tuna",fish_weight:"8lbs",fish_cost:14})
-  instance1.save( function(err,doc) {
-  if(err) return console.error(err);
-  console.log("First object saved")
+  let instance1 = new fish({ fish_type: "Salman", fish_weight: "2lbs", fish_cost: 34 });
+  let instance2 = new fish({ fish_type: "Sardine", fish_weight: "6lbs", fish_cost: 23 })
+  let instance3 = new fish({ fish_type: "Tuna", fish_weight: "8lbs", fish_cost: 14 })
+  instance1.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("First object saved")
   });
-  
-  instance2.save( function(err,doc) {
-      if(err) return console.error(err);
-      console.log("Second object saved")
-      });
-  
-  instance3.save( function(err,doc) {
-      if(err) return console.error(err);
-      console.log("Third object saved")
-          });
-  }
-  let reseed = true;
-  if (reseed) { recreateDB();}
+
+  instance2.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Second object saved")
+  });
+
+  instance3.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Third object saved")
+  });
+}
+let reseed = true;
+if (reseed) { recreateDB(); }
 
 require('dotenv').config();
 const connectionString =
-process.env.MONGO_CON
+  process.env.MONGO_CON
 mongoose = require('mongoose');
 mongoose.connect(connectionString,
-{useNewUrlParser: true,
-useUnifiedTopology: true});
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 
 //Get the default connection
 var db = mongoose.connection;
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")});
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
+});
 var app = express();
 
 
@@ -73,12 +76,12 @@ app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -89,3 +92,5 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
--- controllers/fish.js
+++ controllers/fish.js
@@ -1,65 +1,76 @@
 var fish = require('../models/fish');
 // List of all fishes
-exports.fish_list = async function(req, res) {
-    try{
-    thefishes = await fish.find();
-    res.send(thefishes);
+exports.fish_list = async function (req, res) {
+    try {
+        thefishes = await fish.find();
+        res.send(thefishes);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-    };
+};
 // List of all fishes
 //exports.fish_list = function(req, res) {
 //res.send('NOT IMPLEMENTED: fish list');
 //};
 // for a specific fish.
-exports.fish_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: fish detail: ' + req.params.id);
-};
+//exports.fish_detail = function (req, res) {
+   // res.send('NOT IMPLEMENTED: fish detail: ' + req.params.id);
+//};
 // Handle fish create on POST.
-exports.fish_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: fish create POST');
+exports.fish_create_post = function (req, res) {
+    res.send('NOT IMPLEMENTED: fish create POST');
 };
 // Handle fish delete form on DELETE.
-exports.fish_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: fish delete DELETE ' + req.params.id);
+exports.fish_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: fish delete DELETE ' + req.params.id);
 };
 // Handle fish update form on PUT.
-exports.fish_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: fish update PUT' + req.params.id);
+exports.fish_update_put = function (req, res) {
+    res.send('NOT IMPLEMENTED: fish update PUT' + req.params.id);
 };
 
 // VIEWS
 // Handle a show all view
-exports.fish_view_all_Page = async function(req, res) {
-    try{
-    thefishes = await fish.find();
-    res.render('fish', { title: 'fish Search Results', results: thefishes });
+exports.fish_view_all_Page = async function (req, res) {
+    try {
+        thefishes = await fish.find();
+        res.render('fish', { title: 'fish Search Results', results: thefishes });
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-    };
-    // Handle Costume create on POST.
-exports.fish_create_post = async function(req, res) {
+};
+// Handle Costume create on POST.
+exports.fish_create_post = async function (req, res) {
     console.log(req.body)
     let document = new fish();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    
+
     document.fish_type = req.body.fish_type;
     document.fish_weight = req.body.fish_weight;
     document.fish_cost = req.body.fish_cost;
-    try{
-    let result = await document.save();
-    res.send(result);
+    try {
+        let result = await document.save();
+        res.send(result);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-    };
+};
+// for a specific fish.
+exports.fish_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await fish.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+};
