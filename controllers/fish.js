86a5dff254ae954968ba415a69ec6dcbf4d9a01f
AnilkumarPalavelli <111616559+AnilkumarPalavelli@users.noreply.github.com>
2022-11-15 22:16:15 -0600

    Added bonus

--- controllers/fish.js
+++ controllers/fish.js
@@ -136,3 +136,31 @@ exports.fish_create_Page = function (req, res) {
         res.send(`{'error': '${err}'}`);
     }
 };
+
+// Handle building the view for updating a fish.
+// query provides the id
+exports.fish_update_Page = async function (req, res) {
+    console.log("update view for item " + req.query.id)
+    try {
+        let result = await fish.findById(req.query.id)
+        res.render('fishupdate', { title: 'fish Update', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle a delete one view with id from query
+exports.fish_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await fish.findById(req.query.id)
+    res.render('fishdelete', { title: 'fish Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
--- routes/fish.js
+++ routes/fish.js
@@ -8,4 +8,8 @@ router.get('/', fish_controlers.fish_view_all_Page);
 router.get('/detail', fish_controlers.fish_view_one_Page);
 /* GET create fish page */
 router.get('/create', fish_controlers.fish_create_Page);
+/* GET create update page */
+router.get('/update', fish_controlers.fish_update_Page);
+/* GET delete fish page */
+router.get('/delete', fish_controlers.fish_delete_Page);
 module.exports = router;
