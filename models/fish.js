ce6502c360b64b0938b901f01ce9b2f4509b242f
AnilkumarPalavelli <111616559+AnilkumarPalavelli@users.noreply.github.com>
2022-11-29 19:36:25 -0600

    Updated the fish file in the models

--- models/fish.js
+++ models/fish.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose")
 const fishSchema = mongoose.Schema({
 fish_type: {type:String, maxLength:10},
 fish_weight: String,
-fish_cost: Number {type:Number, max:50}
+fish_cost:  {type:Number, max:50}
 })
 module.exports = mongoose.model("fish",fishSchema)
